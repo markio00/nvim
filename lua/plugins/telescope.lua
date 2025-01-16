c92c8c66bd59f9445ec77505ae8d4059d2556dce
Marco <marco.marchiante@gmail.com>
2025-01-17 00:45:37 +0100

    added telescope bindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,6 +17,7 @@ return {
         pcall(require("telescope").load_extension, "fzf")
 
         local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
         vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
         vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
         vim.keymap.set("n", "<leader>/", function()
